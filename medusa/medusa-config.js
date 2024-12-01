ed785a4240fd4a50d70b8b611a6e805d182788d3
“shubhamchoudhary-2003” <shubhamera484@gmail.com>
2024-12-01 09:39:21 +0530

    first commit

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -19,47 +19,153 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
     },
   },
+  // modules: [
+  //   {
+  //     resolve: '@medusajs/medusa/payment',
+  //     options: {
+  //       providers: [
+  //         {
+  //           id: 'stripe',
+  //           resolve: '@medusajs/medusa/payment-stripe',
+  //           options: {
+  //             apiKey: process.env.STRIPE_API_KEY,
+  //             webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     resolve: './src/modules/fashion',
+  //   },
+  //   {
+  //     resolve: '@medusajs/medusa/file',
+  //     options: {
+  //       providers: [
+  //         {
+  //           resolve: '@medusajs/medusa/file-s3',
+  //           id: 's3',
+  //           options: {
+  //             file_url: process.env.S3_FILE_URL,
+  //             access_key_id: process.env.S3_ACCESS_KEY_ID,
+  //             secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+  //             region: process.env.S3_REGION,
+  //             bucket: process.env.S3_BUCKET,
+  //             endpoint: process.env.S3_ENDPOINT,
+  //             additional_client_config: {
+  //               forcePathStyle:
+  //                 process.env.S3_FORCE_PATH_STYLE === 'true' ? true : undefined,
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // ],
+
   modules: [
     {
-      resolve: '@medusajs/medusa/payment',
-      options: {
-        providers: [
-          {
-            id: 'stripe',
-            resolve: '@medusajs/medusa/payment-stripe',
-            options: {
-              apiKey: process.env.STRIPE_API_KEY,
-              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: './src/modules/fashion',
-    },
-    {
-      resolve: '@medusajs/medusa/file',
-      options: {
-        providers: [
-          {
-            resolve: '@medusajs/medusa/file-s3',
-            id: 's3',
-            options: {
-              file_url: process.env.S3_FILE_URL,
-              access_key_id: process.env.S3_ACCESS_KEY_ID,
-              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-              region: process.env.S3_REGION,
-              bucket: process.env.S3_BUCKET,
-              endpoint: process.env.S3_ENDPOINT,
-              additional_client_config: {
-                forcePathStyle:
-                  process.env.S3_FORCE_PATH_STYLE === 'true' ? true : undefined,
-              },
-            },
-          },
-        ],
-      },
-    },
-  ],
+       resolve: './src/modules/fashion',
+     },
+ 
+   {
+     resolve: "@medusajs/medusa/fulfillment",
+     options: {
+       providers: [
+         {
+           resolve: `@medusajs/medusa/fulfillment-manual`,
+           id: "manual",
+           options: {
+             // provider options...
+           },
+         },]
+       } ,
+     },
+   {
+     resolve: "@medusajs/medusa/cache-redis",
+     options: {
+       redisUrl: process.env.REDIS_URL,
+     },
+   },
+   {
+     resolve: "@medusajs/medusa/event-bus-redis",
+     options: {
+       redisUrl: process.env.REDIS_URL,
+     },
+   },
+   {
+     resolve: "@medusajs/medusa/workflow-engine-redis",
+     options: {
+       redis: {
+         url: process.env.REDIS_URL,
+       },
+     },
+   },
+
+   {
+     resolve: '@medusajs/medusa/payment',
+     options: {
+       providers: [
+         {
+           id: 'stripe',
+           resolve: '@medusajs/medusa/payment-stripe',
+           options: {
+             apiKey: process.env.STRIPE_API_KEY,
+             webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+           },
+         }, {
+           resolve: "@sgftech/payment-razorpay",
+           id: "razorpay",
+           options: {
+             key_id:
+                 process?.env?.RAZORPAY_TEST_KEY_ID ??
+                 process?.env?.RAZORPAY_ID,
+             key_secret:
+                 process?.env?.RAZORPAY_TEST_KEY_SECRET ??
+                 process?.env?.RAZORPAY_SECRET,
+             razorpay_account:
+                 process?.env?.RAZORPAY_TEST_ACCOUNT ??
+                 process?.env?.RAZORPAY_ACCOUNT,
+             automatic_expiry_period: 30 /* any value between 12minuts and 30 days expressed in minutes*/,
+             manual_expiry_period: 20,
+             refund_speed: "normal",
+             webhook_secret:
+                 process?.env?.RAZORPAY_TEST_WEBHOOK_SECRET ??
+                 process?.env?.RAZORPAY_WEBHOOK_SECRET
+         }
+         },
+       ],
+     },
+    
+   },
+
+   {
+     resolve: './src/modules/fashion',
+   },
+   {
+     resolve: '@medusajs/medusa/file',
+     options: {
+       providers: [
+         {
+           resolve: '@medusajs/medusa/file-s3',
+           id: 's3',
+           options: {
+             file_url: process.env.S3_FILE_URL,
+             access_key_id: process.env.S3_ACCESS_KEY_ID,
+             secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+             region: process.env.S3_REGION,
+             bucket: process.env.S3_BUCKET,
+             endpoint: process.env.S3_ENDPOINT,
+             additional_client_config: {
+               forcePathStyle:
+                 process.env.S3_FORCE_PATH_STYLE === 'true' ? true : undefined,
+             },
+           },
+         },
+       ],
+     },
+   },
+ ],
+
+
 });
